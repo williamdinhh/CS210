9920b52e8c1fd8cd79658b5ff202734ac0dff9fd
William <billdinh52@gmail.com>
2024-04-04 19:18:30 -0700

    test

--- src/Week1/exerciseOne.java
+++ src/Week1/exerciseOne.java
@@ -3,5 +3,6 @@ package Week1;
 public class exerciseOne {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Updates 2");
     }
 }
